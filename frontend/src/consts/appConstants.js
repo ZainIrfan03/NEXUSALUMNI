0623b09317787fd9cd5a187107f6ea043788c1c7
ZainIrfan03 <zainirfan4162@gmail.com>
2026-08-20 15:31:22 +0500

    FRONTEND AND BACKEND OPTIMIZATION

--- frontend/src/consts/appConstants.js
+++ frontend/src/consts/appConstants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; 
+ export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; 
  export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const SOCKET_EVENTS = {
